f918d047adbcdeadf36f3a654b0df77aa9c3f41b
huyns1998 <syhuynguyen1@gmail.com>
2022-01-10 15:19:43 +0700

    assignment2

---
+++ Assignment2/Debug/Src/main.d
@@ -0,0 +1,46 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h \
+ C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h \
+ C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/eventman.h \
+ C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Utilities/buff.h \
+ C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/led/led.h \
+ C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/buzzer/melody.h \
+ C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/buzzer/buzzer.h \
+ C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/sensor/lightsensor.h \
+ C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/sensor/temhumsensor.h \
+ C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/button/eventbutton.h \
+ C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/button/button.h \
+ C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/Ucglib.h \
+ C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/ucg.h \
+ C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/serial/uartcmd.h \
+ C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/serial/serial.h
+
+C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h:
+
+C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h:
+
+C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/eventman.h:
+
+C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Utilities/buff.h:
+
+C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/led/led.h:
+
+C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/buzzer/melody.h:
+
+C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/buzzer/buzzer.h:
+
+C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/sensor/lightsensor.h:
+
+C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/sensor/temhumsensor.h:
+
+C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/button/eventbutton.h:
+
+C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/button/button.h:
+
+C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/Ucglib.h:
+
+C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/ucg.h:
+
+C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/serial/uartcmd.h:
+
+C:/Users/admin/Desktop/firmware/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/serial/serial.h:
